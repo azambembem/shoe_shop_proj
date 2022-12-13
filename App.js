2b5907d6bf1890979f285d2c8f2793f1088a9468
azambembem <112090378+azambembem@users.noreply.github.com>
2022-12-13 17:08:20 +0900

    Add files via upload

---
+++ App.js
@@ -0,0 +1,51 @@
+import "./App.css";
+import { useState } from "react";
+import { Navbar, Container, Nav } from "react-bootstrap";
+import data from "./data.js";
+
+function App() {
+  let [shoes] = useState(data);
+
+  return (
+    <div className="App">
+      <Navbar bg="light" variant="light">
+        <Container>
+          <Navbar.Brand href="#home">ShoeShop</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">Cart</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+
+      <div className="main-bg">
+        <img src="https://codingapple1.github.io/shop/shoes5.jpg" width="60%" />
+      </div>
+
+      <div className="container">
+        <div className="row">
+          {shoes.map((a, i) => {
+            return <Card shoes={shoes[i]} i={i}></Card>;
+          })}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function Card(props) {
+  return (
+    <div className="col-md-4">
+      <img
+        src={
+          "https://codingapple1.github.io/shop/shoes" + (props.i + 1) + ".jpg"
+        }
+        width="80%"
+      />
+      <h4>{props.shoes.title}</h4>
+      <p>{props.shoes.price}</p>
+    </div>
+  );
+}
+
+export default App;
---
+++ data.js
@@ -0,0 +1,22 @@
+let data = [
+  {
+    id: 0,
+    title: "White and Black",
+    content: "Born in France",
+    price: "120000won",
+  },
+  {
+    id: 1,
+    title: "Red Knit",
+    content: "Born in Seoul",
+    price: "110000won",
+  },
+  {
+    id: 2,
+    title: "Grey Yordan",
+    content: "Born in  States",
+    price: "130000won",
+  },
+];
+
+export default data;
